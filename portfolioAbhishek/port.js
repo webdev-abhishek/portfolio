f8e300f5cabbcc7cb6ce00bc3af2616068fb7a7f
Abhishek <143903846+webdev-abhishek@users.noreply.github.com>
2023-11-04 17:58:31 +0530

    Add files via upload

---
+++ portfolioAbhishek/port.js
@@ -0,0 +1,10 @@
+var typed = new Typed(".text", {
+    strings: ["Forntend Developer", "UI,Ux Developer", "Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
+
+
